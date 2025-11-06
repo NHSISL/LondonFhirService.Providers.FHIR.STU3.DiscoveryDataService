f242623ec6c9ef7eed1d9f53143ec33ff93a68fe
Joe Owens <joe.owens@answerdigital.com>
2025-11-06 17:19:44 +0000

    FOUNDATIONS: Patient GetStructuredPatient validation test

---
+++ LondonFhirService.Providers.FHIR.STU3.DiscoveryDataService.Tests.Unit/Services/Patients/PatientServiceTests.GetStructuredPatient.Validations.cs
@@ -0,0 +1,46 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Threading.Tasks;
+using FluentAssertions;
+using Hl7.Fhir.Model;
+using LondonFhirService.Providers.FHIR.STU3.DiscoveryDataService.Models.Services.Patients.Exceptions;
+using Task = System.Threading.Tasks.Task;
+
+namespace LondonFhirService.Providers.FHIR.STU3.DiscoveryDataService.Tests.Unit.Services.Patients
+{
+    public partial class PatientServiceTests
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("   ")]
+        public async Task ShouldThrowValidationExceptionOnGetStructuredPatientAsync(string invalidText)
+        {
+            // given
+            var invalidArgumentPatientServiceException =
+                new InvalidArgumentPatientServiceException(
+                    "Invalid patient service argument, please correct the errors and try again.");
+
+            invalidArgumentPatientServiceException.AddData(
+                key: "Id",
+                values: "Text is required");
+
+            var expectedPatientValidationException =
+                new PatientValidationException(
+                    message: "Patient validation error occurred, please fix the errors and try again.",
+                    innerException: invalidArgumentPatientServiceException);
+
+            // when
+            ValueTask<Bundle> getStructuredPatientAsyncAction =
+                patientService.GetStructuredPatientAsync(invalidText);
+
+            PatientValidationException actualException =
+                await Assert.ThrowsAsync<PatientValidationException>(getStructuredPatientAsyncAction.AsTask);
+
+            // then
+            actualException.Should().BeEquivalentTo(expectedPatientValidationException);
+        }
+    }
+}
