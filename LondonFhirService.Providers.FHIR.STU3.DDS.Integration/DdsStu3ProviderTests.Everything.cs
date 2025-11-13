2dc261e6df15507d1eff98859e565a713a838c8a
Joe Owens <joe.owens@answerdigital.com>
2025-11-13 15:44:29 +0000

    INTEGRATIONS: Everything

---
+++ LondonFhirService.Providers.FHIR.STU3.DDS.Integration/DdsStu3ProviderTests.Everything.cs
@@ -0,0 +1,21 @@
+// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Threading.Tasks;
+
+namespace LondonFhirService.Providers.FHIR.STU3.DDS.Integration
+{
+    public partial class DdsStu3ProviderTests
+    {
+        [Fact]
+        public async Task ShouldEverythingAsync()
+        {
+            // given
+            string inputId = "9435797881";
+
+            // when
+            await this.ddsStu3Provider.Patients.Everything(id: inputId);
+        }
+    }
+}
